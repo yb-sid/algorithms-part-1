00bcb1561e078b0ff947ad604989ac1f7a6874b9
Siddharth Kumar <ybsid@Siddharths-MacBook-Pro.local>
2023-07-30 16:03:15 +0530

    week-1 , code Union-find :: all iterations

---
+++ src/week1/union_find/QuickFindUF.java
@@ -0,0 +1,31 @@
+package week1.union_find;
+
+public class QuickFindUF {
+
+    private int[] ids;
+
+    public QuickFindUF(int N){
+        ids = new int[N];
+        for(int i=0;i<N;i++){
+            ids[i] = i;
+        }
+    }
+
+    // find operation
+    public boolean connected(int p , int q ){
+        return ids[p] == ids[q];
+    }
+
+    // merge two components
+    // N union operation - takes N^2
+    public void union(int p , int q ){
+        int pid = ids[p];
+        int qid = ids[q];
+
+        for(int i=0;i<ids.length;i++){
+            if(ids[i]==pid){
+                ids[i] = qid;
+            }
+        }
+    }
+}
