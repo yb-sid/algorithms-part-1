3fb6a4d28a5eada5f9aeedb5131a1407fb827fe3
Siddharth Kumar <ybsid@Siddharths-MacBook-Pro.local>
2023-08-12 14:13:00 +0530

    test simple sorts from week2

--- src/week2/basic_sorting/Insertion.java
+++ src/week2/basic_sorting/Insertion.java
@@ -1,11 +1,15 @@
 package week2.basic_sorting;
 
+import javax.xml.transform.stream.StreamSource;
+import java.util.Arrays;
+
 public class Insertion {
 
     public void sort(Comparable[] a){
         int N = a.length;
         for(int i=0;i<N;i++){
             for(int j=i;j>0;j--){
+                System.out.println("j value is : "+j);
                 if(less(a[j] , a[j-1])){
                     exchange(a , j , j -1);
                 }else break;
@@ -21,5 +25,13 @@ public class Insertion {
         Comparable temp = a[i];
         a[i] = a[j];
         a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Insertion insertionSort = new Insertion();
+        String[] arr = new String[]{"bb","aa","cc","gg","zz","kk","mm"};
+        System.out.println("Array before insertion sort : "+ Arrays.toString(arr));
+        insertionSort.sort(arr);
+        System.out.println("Array after insertion sort : "+ Arrays.toString(arr));
     }
 }
